11382d115e248fe4026795e7ff83d348eb75aaea
Santiago Maradona <pepioca@gmail.com>
2018-05-27 19:22:09 -0300

    Init repo

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  def hello
+    render html: "Hello Poper!"
+  end
+
+  def goodbye
+    render html: "Goodbye Poper!"
+  end
+end
